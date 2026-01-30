d0071a67f4b0e0efd7a02cfe17934aa07d48db4a
TrishonParrilla <trishonp1@gmail.com>
2026-01-29 18:23:24 -0700

    Fix conversion function to accept fahrenheit parameter

--- scripts/tempconversion.js
+++ scripts/tempconversion.js
@@ -1,7 +1,6 @@
-function conversion() {
+function conversion(fahrenheit=0) {
     let celcius = prompt("Enter Celcius Temperature: ");
     fahrenheit = celcius * 9/5 + 32;
     document.getElementById("temp-calculation").innerHTML = 
     `The temperature is ${fahrenheit} °F`;
-    
 }
