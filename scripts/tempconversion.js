e6b1af05d82a54ea4bc6c11d1916865199d767af
TrishonParrilla <trishonp1@gmail.com>
2026-01-29 17:50:47 -0700

    Add HTML structure, CSS styles, and JavaScript for temperature conversion

---
+++ scripts/tempconversion.js
@@ -0,0 +1,7 @@
+function conversion() {
+    let celcius = prompt("Enter Celcius Temperature: ");
+    fahrenheit = celcius * 9/5 + 32;
+    document.getElementById("temp-calculation").innerHTML = 
+    `The temperature is ${fahrenheit} °F`;
+    
+}
